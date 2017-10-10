c24835d69bce391fed89f56ae6a491e48c14bcfa
Nekomon <marilia_yonekawa@hotmail.com>
2017-10-09 23:21:35 -0300

    inicio rpg

---
+++ Assets/PlayerMotor.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+[RequireComponent(typeof(NavMeshAgent))]
+public class PlayerMotor : MonoBehaviour {
+
+    NavMeshAgent agente;
+
+	// Use this for initialization
+	void Start () {
+        agente = GetComponent<NavMeshAgent>();
+	}
+	
+	
+    public void MoveToPoint (Vector3 point)
+    {
+        agente.SetDestination(point);
+    }
+	
+}
